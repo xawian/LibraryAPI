2596c3727660b6b85027c7a4daf7fb8c28c1a7d9
xawian <66998201+xawian@users.noreply.github.com>
2022-11-29 00:20:29 +0100

    edit

--- base/models.py
+++ base/models.py
@@ -85,13 +85,6 @@ class Assesment(models.Model):
     assesment = models.PositiveIntegerField()
 
 
-class Submissions(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=35)
-    message = models.CharField(max_length=30,null=True, blank=True)
-
-    def __str__(self):
-        return self.email
 
 
 
